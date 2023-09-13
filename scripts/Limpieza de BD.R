09d30f97b8f96cae4a0c174b77fa70e1b909d174
Javier Farfán López <dj.farfan10@uniandes.edu.co>
2023-09-12 21:56:22 -0500

    Update Limpieza de BD.R

--- scripts/Limpieza de BD.R
+++ scripts/Limpieza de BD.R
@@ -12,3 +12,19 @@ df<- database_18
 
 #Analizando la estructura de la base de datos
 glimpse(df)
+
+##Se seleccionan las variables de interés
+
+##age, clase, college, cuentapropia, dsi, estrato1, hoursWorkUsal, informal, ingtotob, maxEducLevel
+##microEmpresa, ocu, oficio, p6210, p6620s1, relab, sex, sizeFirm, y_bonificacio~m, y_salarySec_m, y_ingLab_m_ha, 
+##y_total_m, y_total_m_ha
+
+df<- df %>%
+  select(age, clase, college, cuentaPropia, dsi, estrato1, hoursWorkUsual, informal, ingtotob, maxEducLevel, microEmpresa, ocu, oficio, p6210, p6620s1, relab, sex, sizeFirm, y_bonificaciones_m, y_salarySec_m, y_ingLab_m_ha, y_total_m, y_total_m_ha)
+
+show(df)
+
+##Se verifican los NAs de la base
+
+
+
