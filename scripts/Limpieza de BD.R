4eaa31c635477e684784e889856bf694ce4a2c81
Javier Farfán López <dj.farfan10@uniandes.edu.co>
2023-09-13 20:10:23 -0500

    Limpieza de base de datos lista

--- scripts/Limpieza de BD.R
+++ scripts/Limpieza de BD.R
@@ -51,12 +51,52 @@ df_sin_nan<- df[!is.na(df$informal), ]
 
 sapply(df_sin_nan, function(x) sum(is.na(x)))
 
+df_sin_nan<- df_sin_nan[!is.na(df_sin_nan$y_total_m_ha), ]
+sapply(df_sin_nan, function(x) sum(is.na(x)))
 
+df_sin_nan<- df_sin_nan[!is.na(df_sin_nan$maxEducLevel), ]
+sapply(df_sin_nan, function(x) sum(is.na(x)))
 
+df_sin_nan<- df_sin_nan %>%
+  select(age, clase, college, cuentaPropia, dsi, estrato1, hoursWorkUsual, informal, maxEducLevel, microEmpresa, ocu, oficio, relab, sex, sizeFirm, y_total_m, y_total_m_ha)
 
+##Limpieza por outliers
 
+std_wageh<- sd(df_sin_nan$y_total_m_ha)
+print(std_wageh)
+tres_std_wageh<- std_wageh*3
+print(tres_std_wageh)
 
+df_clean <- df_sin_nan[df_sin_nan$y_total_m_ha <= tres_std_wageh, ]
 
+##Creación del logaritmo del salario
 
+df_clean$log_wageh <- log(df_clean$y_total_m_ha)
 
+#Creación de variable female, en la que hombre es la base
 
+df_clean$female <- 0
+df_clean$female <- ifelse(df_clean$sex==0, 1, df_clean$female)
+
+##Verificando la variable female
+
+veri_female <- df_clean[,c("sex","female")]
+print(veri_female)
+
+##Creación de edad al cuadrado
+
+df_clean$age2 <- (df_clean$age)^2
+
+##Se convierten en factores las variables categoricas
+
+variables_categoricas <- c("estrato1", "maxEducLevel", "oficio", "relab", "sizeFirm")
+df_clean<- df_clean %>%
+  mutate_at(variables_categoricas, as.factor)
+
+##Verificando el tipo de variable para asegurar que sea lo indicado (String, otro)
+
+glimpse(df_clean)
+
+##Guardando la base de datos limpia
+
+save(df_clean, file = "d:/Javier/Desktop/UNIANDES/Big Data/Taller_1/stores/database_18_clean.Rda")
